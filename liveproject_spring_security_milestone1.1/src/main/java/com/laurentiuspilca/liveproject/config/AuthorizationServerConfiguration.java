4acd543026467fc6807fd432494232998c2dad17
Laurentiu Spilca <Laurentiu.Spilca@endava.com>
2020-08-27 14:33:16 +0300

    Splitting milestone 1 in 3 parts

---
+++ liveproject_spring_security_milestone1.1/src/main/java/com/laurentiuspilca/liveproject/config/AuthorizationServerConfiguration.java
@@ -0,0 +1,47 @@
+package com.laurentiuspilca.liveproject.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+
+@Configuration
+@EnableAuthorizationServer
+public class AuthorizationServerConfiguration
+        extends AuthorizationServerConfigurerAdapter {
+
+  private final AuthenticationManager authenticationManager;
+
+  private final TokenStore tokenStore;
+
+  private final JwtAccessTokenConverter converter;
+
+  public AuthorizationServerConfiguration(AuthenticationManager authenticationManager,
+                                          TokenStore tokenStore,
+                                          JwtAccessTokenConverter converter) {
+    this.authenticationManager = authenticationManager;
+    this.tokenStore = tokenStore;
+    this.converter = converter;
+  }
+
+  @Override
+  public void configure(AuthorizationServerEndpointsConfigurer endpoints) {
+    endpoints.authenticationManager(authenticationManager)
+      .tokenStore(tokenStore)
+      .accessTokenConverter(converter);
+  }
+
+  @Override
+  public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+    clients.inMemory()
+            .withClient("client")
+            .secret("secret")
+            .authorizedGrantTypes("authorization_code", "password", "refresh_token")
+            .scopes("read");
+  }
+
+}
